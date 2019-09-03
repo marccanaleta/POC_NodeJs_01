d6ad9873afe828d2b762be39ede4aa83571cc899
Javier Mora Chacon <javier.mora@between.tech>
2019-09-03 09:17:55 +0200

    Fix error

---
+++ routes/businessRules.js
@@ -0,0 +1,27 @@
+
+
+exports.getString = function() {
+    return "Hi, how are you?";
+};
+
+//TODO: Develop this functions
+
+exports.suma = function(numA, numB) {
+   
+};
+
+exports.resta = function(numA, numB) {
+
+};
+
+exports.multiplica = function(numA, numB) {
+    
+};
+
+exports.divide = function(numA, numB) {
+
+};
+
+//END TODO;
+
+module.exports = exports;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var app = require('app.js');
 var express = require('express');
 var router = express.Router();
+var businessRules = require('./businessRules.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-router.get('/', function(req, res, next) {
-    res.render('index', app.Get);
+    res.render('index', 
+    { 
+        title: 'Express',    
+        foo: businessRules.getString()
+    })
 });
 
 module.exports = router;
